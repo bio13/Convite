05d4ece02401b8253f51de8d0d8a380057b169a5
bio13 <vi.vieirasg17@gmail.com>
2026-07-14 00:05:58 -0300

    Update game.js

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ setInterval(function() {
 }, 150);
 
 // Inicia o movimento do obstáculo
-obstacle.style.right = "800px"; 
+obstacle.style.right = "1400px"; 
 obstacle.classList.add("obstacle-move");
 
 // 2. LOGICA DO PULO E TROCA DE IMAGENS (Salto e Queda)
